54d96df2f2491caa62a68f9f0c9822fe2259b479
Bernadett Szeiler <bernadett.szeiler@stud.uni-corvinus.hu>
2019-12-02 01:29:04 +0100

    törlés funkció

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -24,6 +24,7 @@ namespace UserMaintenance
             
             button1.Text = Resource1.Add;
             button2.Text = Resource1.Add1;
+            button3.Text = Resource1.Remove;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -55,6 +56,11 @@ namespace UserMaintenance
                 }
                 sw.Close();
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            users.Remove((User)listBox1.SelectedItem);
         }
     }
 }
